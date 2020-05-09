3c8d91735d61abb61e4c4710a6ce02c432cc5609
Nasheb Ismaily <nasheb@gmail.com>
2020-05-09 05:04:10 -0500

    Converted CaptureStream to a class

--- src/main/detection/CaptureStream.py
+++ src/main/detection/CaptureStream.py
@@ -1,23 +1,26 @@
 import cv2 as cv
 
-# get_device_list() enumerates the available cameras connected to the device and
-# returns a list of device id's which can be used with the cv2 VideoCapture construct.
-def get_device_list():
-    device_id = 0
-    device_list = []
-    while True:
-        if cv.VideoCapture(device_id).read()[0]:
-            device_list.append(device_id)
-            device_id += 1
-        else:
-            break
-    return device_list
+# CaptureStream is a utility class for calling the OpenCV2 video capture library
+class CaptureStream:
 
-# get_camera_stream opens the camera specified by the device_id and returns the video stream.
-def get_camera_stream(device_id):
-    return cv.VideoCapture(device_id)
+    # get_device_list() enumerates the available cameras connected to the device and
+    # returns a list of device id's which can be used with the cv2 VideoCapture construct.
+    def get_device_list(self):
+        device_id = 0
+        device_list = []
+        while True:
+            if cv.VideoCapture(device_id).read()[0]:
+                device_list.append(device_id)
+                device_id += 1
+            else:
+                break
+        return device_list
 
-# close_stream stops the video stream from the camera and closes all frame capture windows
-def close_stream(video_capture):
-    video_capture.release()
-    cv.destroyAllWindows()
+    # get_camera_stream opens the camera specified by the device_id and returns the video stream.
+    def get_camera_stream(self,device_id):
+        return cv.VideoCapture(device_id)
+
+    # close_stream stops the video stream from the camera and closes all frame capture windows
+    def close_stream(self,video_capture):
+        video_capture.release()
+        cv.destroyAllWindows()
