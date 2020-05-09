fb444969f2b2c83ea1374e06ec9c184f66e2121d
Nasheb Ismaily <nasheb@gmail.com>
2020-05-08 19:58:19 -0500

    Updated Project Structure

---
+++ src/CaptureStream.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+# get_device_list() enumerates the available cameras connected to the device and
+# returns a list of device id's which can be used with the cv2 VideoCapture construct.
+def get_device_list():
+    device_id = 0
+    device_list = []
+    while True:
+        if cv.VideoCapture(device_id).read()[0]:
+            device_list.append(device_id)
+            device_id += 1
+        else:
+            break
+    return device_list
+
+# get_camera_stream opens the camera specified by the device_id and returns the video stream.
+def get_camera_stream(device_id):
+    return cv.VideoCapture(device_id)
+
+def close_stream(video_capture):
+    video_capture.release()
+    cv.destroyAllWindows()
---
+++ src/Detect.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+from src import CaptureStream as cs
+from src import Enums as e
+
+def main():
+    # These will become input arguments
+    device_id = 0
+    frame_refresh_interval_ms = 1
+
+    device_list = cs.get_device_list()
+    video_capture = cs.get_camera_stream(device_id)
+
+    if video_capture.isOpened():
+        while True:
+            ret_val, frame  = video_capture.read()
+            cv.imshow("device: " + str(device_id), frame)
+
+            interrupt_key = cv.waitKey(frame_refresh_interval_ms)
+            if interrupt_key == e.KeyInterrupt.ESCAPE.value:
+                break
+
+        cs.close_stream()
+
+if __name__ == "__main__":
+    main()
