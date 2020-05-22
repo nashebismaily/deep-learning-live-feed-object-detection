38a1f3781611169c15458693288f228271784d3e
Nasheb Ismaily <nasheb@gmail.com>
2020-05-22 14:01:30 -0500

    Updated tests cases for new class methods

--- test/main/detection/TestCaptureStream.py
+++ test/main/detection/TestCaptureStream.py
@@ -6,16 +6,19 @@ from src.main.detection.CaptureStream import CaptureStream
 # Unit Tests for CaptureStream.py
 class TestCaptureStream(unittest.TestCase):
 
+        # tests scan_devices()
         def test_scan_devices(self):
             capture_stream = CaptureStream()
             self.assertTrue(capture_stream.scan_devices())
 
+        # tests get_camera_stream()
         def test_get_camera_stream(self):
             device_id = 0
             capture_stream = CaptureStream(device_id)
             self.assertIsInstance(capture_stream.get_camera_stream(), cv.VideoCapture)
             capture_stream.close_camera_stream()
 
+        # tests close_stream()
         def test_close_stream(self):
             device_id = 0
             capture_stream = CaptureStream(device_id)
--- test/main/model/TestDarkNet.py
+++ test/main/model/TestDarkNet.py
@@ -4,21 +4,21 @@ from src.main.model import DarkNet as dn
 # Unit Tests for CaptureStream.py
 class TestDarkNet(unittest.TestCase):
 
+        darknet_base_dir = '/root/darknet'
+        darknet_object_data = 'object.data'
+        yolov3_config_file = 'yolov3.cfg'
+        yolov3_weights_file = 'darknet53.conv.74'
+        log_file = 'out.log'
+        graph_file = 'darknetlossgraph.png'
+
+        # tests the constructor for DarkNet()
         def test_default_constructor(self):
-            darknet =  dn.DarkNet()
+            darknet =  dn.DarkNet(self.darknet_base_dir, self.darknet_object_data, self.yolov3_config_file, self.yolov3_weights_file, self.log_file, self.graph_file)
             self.assertEqual(darknet.darknet_base_dir, '/root/darknet')
             self.assertEqual(darknet.darknet_object_data, 'object.data')
             self.assertEqual(darknet.yolov3_config_file, 'yolov3.cfg')
-            self.assertEqual(darknet.yolov3_weights_file, 'darknet53.conv.74')
+            self.assertEqual(darknet.yolov3_train_weights_file, 'darknet53.conv.74')
             self.assertEqual(darknet.log_file, 'out.log')
-
-        def test_default_override_constructor(self):
-            darknet =  dn.DarkNet('a', 'b', 'c', 'd', 'e')
-            self.assertEqual(darknet.darknet_base_dir, 'a')
-            self.assertEqual(darknet.darknet_object_data, 'b')
-            self.assertEqual(darknet.yolov3_config_file, 'c')
-            self.assertEqual(darknet.yolov3_weights_file, 'd')
-            self.assertEqual(darknet.log_file, 'e')
 
 if __name__ == '__main__':
     unittest.main()
