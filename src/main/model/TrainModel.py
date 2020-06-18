5b164af4196de4dea8e3eb4200f11e44200fc318
Nasheb Ismaily <nasheb@gmail.com>
2020-06-18 14:15:17 -0500

    Commented code

--- src/main/model/TrainModel.py
+++ src/main/model/TrainModel.py
@@ -5,14 +5,18 @@ from src.main.utils.Parser import Parser
 # main function
 def main():
     parser = ConfigParser()
+
+    # Read darknet.cfg, darknet section
     d_darknet = Parser.get_config(parser, 'darknet.cfg', 'darknet')
     darknet_base_dir = d_darknet.get('darknet_base_dir')
     darknet_object_data = d_darknet.get('darknet_object_data')
 
+    # Read darknet.cfg, yolov3 section
     d_darknet = Parser.get_config(parser, 'darknet.cfg', 'yolov3')
     yolov3_config_file = d_darknet.get('yolov3_config_file')
     yolov3_train_weights_file = d_darknet.get('yolov3_train_weights_file')
 
+    # Read darknet.cfg, output section
     d_darknet = Parser.get_config(parser, 'darknet.cfg', 'output')
     log_file = d_darknet.get('log_file')
     graph_file = d_darknet.get('graph_file')
@@ -24,8 +28,13 @@ def main():
                       log_file,
                       graph_file)
 
+    # Begin model training
     process = darknet.train()
+
+    # Wait for training to complete
     darknet.wait(process)
+
+    # Plot Neural Network loss graph
     darknet.plot_loss()
 
 if __name__ == "__main__":
